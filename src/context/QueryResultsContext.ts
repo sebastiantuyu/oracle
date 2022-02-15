4d153d4a653625297e07a3fcc28961884bdd620b
sebastiantuyu <alextpineiro@gmail.com>
2022-02-14 22:01:35 -0600

    Add global context:
    * QueryResults handles all the results
    * QuerySearch handles the string requested

---
+++ src/context/QueryResultsContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+import IResultsRow from "../DTOs/DTOs";
+
+
+export const QueryResultsContext = createContext([[] as IResultsRow[],() => {}])
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,28 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryResultsContext } from '../context/QueryResultsContext';
+import { QuerySearchContext } from '../context/QuerySearchContext';
+import IResultsRow from '../DTOs/DTOs';
 import Home from '../pages/Home/Home'
 import Result from '../pages/Result/Result'
+import Search from '../pages/Search/Search';
 
-export default function index() {
+export default function Router() {
+  const [globalQuery, setGlobalQuery]: any = useState("")
+  const [lastResults, setLastResults]: any = useState<IResultsRow[]>([])
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/result/:id" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <QuerySearchContext.Provider value={[globalQuery, setGlobalQuery]}>
+      <QueryResultsContext.Provider value={[lastResults, setLastResults]}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search/:query" element={<Search />}/>
+              <Route path="/result/:id" element={<Result />} />
+            </Routes>
+          </BrowserRouter>
+      </QueryResultsContext.Provider>
+    </QuerySearchContext.Provider>
   )
 }
