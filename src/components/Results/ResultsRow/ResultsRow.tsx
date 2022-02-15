63eb8602339dc9101425c9e87230a008552202e5
sebastiantuyu <alextpineiro@gmail.com>
2022-02-15 13:30:27 -0600

    Add the LoaderWrapper whle image is being loaded

--- src/components/Results/ResultsRow/ResultsRow.tsx
+++ src/components/Results/ResultsRow/ResultsRow.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import IResultsRow from "../../../DTOs/DTOs"
+import LoaderWrapper from '../../LoaderWrapper/LoaderWrapper'
 import "./ResultsRow.scss"
 
 interface IProps {
@@ -9,16 +10,39 @@ interface IProps {
 
 export default function ResultsRow({ data }: IProps) {
   const navigateTo = useNavigate()
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const goToResultPage = () => {
     navigateTo(`/result/${data.id}`)
+  }
+
+  const toggleDisplay = (shouldShow: boolean) => {
+    return shouldShow ? 'block' : 'none'
+  }
+
+  const parseToThumbnail = (url: string): string => {
+    const splitedURL = url.split('/upload/')
+    if(splitedURL.length > 1) {
+      return splitedURL[0] + '/upload/w_150/' + splitedURL[1]
+    }
+    return url;
   }
 
   return (
     <div onClick={() => goToResultPage()}
       className="odl-resultsrow vertical--center">
         <div className="odl-resultsrow__thumbnail">
-            <img className="odl-resultsrow__thumbnail--img" src={data.photo} alt={data.shortDescription} />
+            <img
+              style={{ display: toggleDisplay(isLoaded)}}
+              className="odl-resultsrow__thumbnail--img"
+              onLoad={() => setIsLoaded(true)}
+              src={parseToThumbnail(data.photo)}
+              alt={data.shortDescription}
+            />
+            <LoaderWrapper
+              loading={true}
+              style={{ width: '100%', height: '100%', margin: 0, borderRadius: 0,  display: toggleDisplay(!isLoaded) }}
+            ><></></LoaderWrapper>
         </div>
         <div className="odl-resultsrow__title vertical--center">
             <p>{data.title}</p>
